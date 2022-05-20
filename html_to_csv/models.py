4e5e68d702c1988d8e6bcad855feaecf095aae0c
saadas <saadsheikh182000@gmail.com>
2022-05-19 23:58:22 -0700

    models created for courses

--- html_to_csv/models.py
+++ html_to_csv/models.py
@@ -1,3 +1,12 @@
+from calendar import day_abbr
+import code
+from tkinter.tix import DisplayStyle
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    code = models.IntegerField(max_length=6)
+    days = models.CharField(max_length=20)
+    starttime = models.IntegerField(max_length=4)
+    endtime = models.IntegerField(max_length=4)
+    location = models.CharField(max_length=15)
